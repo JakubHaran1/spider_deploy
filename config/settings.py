1f51489b21aa3eb38a442bcfcc096f489a79d151
jakubharan1 <jakub.haran02@gmail.com>
2026-07-19 07:38:08 +0200

    deploy

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1","spider-deploy.onrender.com"]
 
 
 # Application definition
